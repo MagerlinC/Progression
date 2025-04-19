5195f00b469a01b301a14692501966e857361329
Mikkel Agerlin <ma@complycloud.com>
2025-04-19 13:01:50 +0200

    feat: initial setup

---
+++ src/firebaseconfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "progression-2d140.firebaseapp.com",
+  projectId: "progression-2d140",
+  storageBucket: "progression-2d140.firebasestorage.app",
+  messagingSenderId: "66462119481",
+  appId: "1:66462119481:web:1676a7cc0573adc06bffe8",
+};
