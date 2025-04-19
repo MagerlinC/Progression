90f9c36288801dd53eac64cb56180e1529f9a2ce
Mikkel Agerlin <ma@complycloud.com>
2025-04-19 13:46:01 +0200

    feat: google auth works

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useUserSession } from "@/hooks/use-user-session";
+import { signInWithGoogle, signOutWithGoogle } from "@/lib/firebase/auth";
+import { createSession, removeSession } from "@/actions/auth-actions";
+
+export function Header({ session }: { session: string | null }) {
+  const [userSessionId, user] = useUserSession(session);
+
+  const handleSignIn = async () => {
+    const userUid = await signInWithGoogle();
+    if (userUid) {
+      await createSession(userUid);
+    }
+  };
+
+  const handleSignOut = async () => {
+    await signOutWithGoogle();
+    await removeSession();
+  };
+
+  if (!userSessionId) {
+    return (
+      <header>
+        <button onClick={handleSignIn}>Sign In</button>
+      </header>
+    );
+  }
+
+  return (
+    <header>
+      <h1>Welcome, {user ? user.displayName : "..."}!</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Menu A</a>
+          </li>
+          <li>
+            <a href="#">Menu B</a>
+          </li>
+          <li>
+            <a href="#">Menu C</a>
+          </li>
+        </ul>
+      </nav>
+      <button onClick={handleSignOut}>Sign Out</button>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/hooks/use-user-session.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+import { onAuthStateChanged } from "../lib/firebase/auth";
+import { User } from "firebase/auth";
+
+export function useUserSession(InitSession: string | null) {
+  const [user, setUser] = useState<User | null>(null);
+  const [userUid, setUserUid] = useState<string | null>(InitSession);
+
+  // Listen for changes to the user session
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(async (authUser) => {
+      if (authUser) {
+        setUserUid(authUser.uid);
+        setUser(authUser);
+      } else {
+        setUserUid(null);
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return [userUid, user] as const;
+}
